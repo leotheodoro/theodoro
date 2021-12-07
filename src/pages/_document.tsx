606945a1d6b3921a441d2a2e61a9c5bc85b4b860
Leonardo Theodoro <leonardo@recruitrobin.com>
2021-12-06 22:15:59 -0300

    meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png" />
           <meta name="theme-color" content="#ffffff"></meta>
+          <meta name="description" content="Hello I'm Leonardo Theodor, Software engineer and passionate about life." />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         </Head>
