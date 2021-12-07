1dd4cb5bfbb6dd98a4c5b8714045f57479316c0a
Alexandre Figueiredo <alexandre.figueiredo@parceiro.mrv.com.br>
2021-12-07 14:56:29 -0300

    Front improvements

--- src/components/AsideImage/index.tsx
+++ src/components/AsideImage/index.tsx
@@ -6,6 +6,7 @@ import { FiTwitter, FiInstagram, FiGithub } from 'react-icons/fi'
 export function AsideImage() {
   return (
     <aside className={styles.box}>
+      <h1 className={styles.title}>THEODORO<span className={styles.textRegular}>.DEV</span></h1>
       <h1><Image src="/images/photo.jpg" alt="Leonardo Theodoro" className={styles.photo} width={400} height={400} /></h1>
       <div className={styles.icons}>
         <a href="https://twitter.com/thdrps" target="_blank" rel="noreferrer" aria-label="Twitter"><FiTwitter size={30} /></a>
