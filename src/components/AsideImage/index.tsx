0c3629f1a584f250cb7f9c8910e61abf5d587009
Leonardo Theodoro <leonardo@recruitrobin.com>
2021-12-06 22:09:17 -0300

    width and height to Image

--- src/components/AsideImage/index.tsx
+++ src/components/AsideImage/index.tsx
@@ -6,7 +6,7 @@ import { FiTwitter, FiInstagram, FiGithub } from 'react-icons/fi'
 export function AsideImage() {
   return (
     <aside className={styles.box}>
-      <h1><Image src="/images/photo.jpg" alt="Leonardo Theodoro" className={styles.photo} /></h1>
+      <h1><Image src="/images/photo.jpg" alt="Leonardo Theodoro" className={styles.photo} width={400} height={400} /></h1>
       <div className={styles.icons}>
         <a href="https://twitter.com/thdrps" target="_blank" rel="noreferrer"><FiTwitter size={30} /></a>
         <a href="https://www.instagram.com/leotheodoro_" target="_blank" rel="noreferrer"><FiInstagram size={30} /></a>
