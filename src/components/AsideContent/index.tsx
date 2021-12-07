50f552dcaf56d7c2849cbbc6062ac879e453e8aa
Leonardo Theodoro <leonardo@recruitrobin.com>
2021-12-06 22:00:25 -0300

    &apos instead of '

--- src/components/AsideContent/index.tsx
+++ src/components/AsideContent/index.tsx
@@ -4,7 +4,7 @@ export function AsideContent() {
   return (
     <aside className={styles.box}>
       <h1 className={styles.title}>THEODORO<span className={styles.textRegular}>.DEV</span></h1>
-      <h2 className={styles.marginBottom}><span className={styles.textRegular}>Hey, I'm</span> Leonardo Theodoro</h2>
+      <h2 className={styles.marginBottom}><span className={styles.textRegular}>Hey, I&apos;m</span> Leonardo Theodoro</h2>
       <h2>Software Engineer <span className={styles.textRegular}>and <br />passionate about</span> life</h2>
     </aside>
   )
