9c8e4089fd3d4e974c77bca2b41d40989b5e1acb
Leonardo Theodoro <leonardo@recruitrobin.com>
2021-12-06 22:06:59 -0300

    imagr 400x400 and using next/image

--- src/components/AsideImage/index.tsx
+++ src/components/AsideImage/index.tsx
@@ -1,10 +1,12 @@
 import styles from './styles.module.scss'
+import Image from "next/image"
 import { FiTwitter, FiInstagram, FiGithub } from 'react-icons/fi'
+
 
 export function AsideImage() {
   return (
     <aside className={styles.box}>
-      <h1><img src="/images/photo.jpg" alt="Leonardo Theodoro" className={styles.photo} /></h1>
+      <h1><Image src="/images/photo.jpg" alt="Leonardo Theodoro" className={styles.photo} /></h1>
       <div className={styles.icons}>
         <a href="https://twitter.com/thdrps" target="_blank" rel="noreferrer"><FiTwitter size={30} /></a>
         <a href="https://www.instagram.com/leotheodoro_" target="_blank" rel="noreferrer"><FiInstagram size={30} /></a>
