8c3966c9cabb6f022fd8d9474c5c710e2b658382
Leonardo Theodoro <leonardo@recruitrobin.com>
2021-12-06 22:19:17 -0300

    Links must have discernible text

--- src/components/AsideImage/index.tsx
+++ src/components/AsideImage/index.tsx
@@ -8,9 +8,9 @@ export function AsideImage() {
     <aside className={styles.box}>
       <h1><Image src="/images/photo.jpg" alt="Leonardo Theodoro" className={styles.photo} width={400} height={400} /></h1>
       <div className={styles.icons}>
-        <a href="https://twitter.com/thdrps" target="_blank" rel="noreferrer"><FiTwitter size={30} /></a>
-        <a href="https://www.instagram.com/leotheodoro_" target="_blank" rel="noreferrer"><FiInstagram size={30} /></a>
-        <a href="https://github.com/leotheodoro" target="_blank" rel="noreferrer"><FiGithub size={30} /></a>
+        <a href="https://twitter.com/thdrps" target="_blank" rel="noreferrer" aria-label="Twitter"><FiTwitter size={30} /></a>
+        <a href="https://www.instagram.com/leotheodoro_" target="_blank" rel="noreferrer" aria-label="Instagram"><FiInstagram size={30} /></a>
+        <a href="https://github.com/leotheodoro" target="_blank" rel="noreferrer" aria-label="Github"><FiGithub size={30} /></a>
       </div>
     </aside >
   )
